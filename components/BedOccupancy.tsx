31fec661365ecb65bb10b8c2176789b4a505a116
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2025-04-06 20:40:05 +0530

    bed occupancy card

--- components/BedOccupancy.tsx
+++ components/BedOccupancy.tsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React from "react";
+import DonutChart from "./DonutChart";
 
 const BedOccupancy = () => {
   return (
-    <div className='h-full w-full border-1 border-gray-300 rounded-lg'>
-      <div className='text-black text-xl font-bold px-6 py-2 border-b border-gray-300'>
+    <div className="h-full w-full border-1 border-gray-300 rounded-lg">
+      <div className="text-black text-xl font-bold px-6 py-2 border-b border-gray-300">
         Total Bed Occupancy
       </div>
-      <div className='grid grid-rows-3 gap-2'>
-        <div className='grid grid-cols-2 gap-2'>
-          <div className='bg-red-400 w-full h-full'>
-            <div className='text-gray-400 text-center'>
-                Occupied Beds
-            </div>
-          </div>
-          <div className='bg-blue-500 rounded-lg w-full h-full'></div>
-          </div>
+      <div className="grid grid-cols-3 gap-2 h-full w-full">
+        {/* Text section */}
+        <div className="grid grid-row-2 col-span-1 gap-2 h-full w-full justify-center items-center">
+          <StatText text="Occupied" value={0} />
+          <StatText text="Unoccupied" value={0} />
+        </div>
+        {/* Semi Donut Chart */}
+        <div className="col-span-2 h-full w-full justify-center items-center">
+          <DonutChart />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BedOccupancy
+export default BedOccupancy;
+
+export const StatText = (props: any) => {
+    return(
+        <div className="grid grid-row-2 items-center justify-start w-full h-full gap-2">
+            <div className="flex justify-start items-center text-black text-2xl w-full h-full gap-2">
+              <div className={ "w-1 h-4 rounded-lg" +(props.text === "Occupied" ? " bg-green-200" : " bg-red-300")}></div>
+              <div className="text-gray-400 text-lg text-center">
+                {props.text} Beds
+              </div>
+            </div>
+              <div className="flex justify-start items-center text-black text-2xl w-full h-full gap-2">
+                <span className="font-extrabold text-3xl px-1">{props.value}</span> Beds
+              </div>
+          </div>
+    );
+}
 // // /* Donut Chart */
 
 // /* Vector 965 */
@@ -35,4 +53,3 @@ export default BedOccupancy
 // flex: none;
 // order: 0;
 // flex-grow: 0;
-
